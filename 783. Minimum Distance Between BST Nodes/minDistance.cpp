ade4b95729d66de1b6fb4871469bc9a57f6eff36
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-02-17 23:10:13 +0530

    Create minDistance.cpp

---
+++ 783. Minimum Distance Between BST Nodes/minDistance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+void solve(TreeNode* root, vector<int> &ans)
+{
+    if(root == NULL)
+    return;
+    else
+    {
+        solve(root->left,ans);
+        ans.push_back(root->val);
+        solve(root->right,ans);
+    }
+}
+    int minDiffInBST(TreeNode* root) {
+        if(root == NULL)
+        return 0;
+        vector<int> ans;
+        solve(root,ans);
+        int mini=INT_MAX;
+        for(int i=0;i<ans.size()-1;i++)
+        {
+            mini=min(mini,ans[i+1]-ans[i]);
+        }
+        return mini;
+    }
+};
