2c47f247f98379b2c750283eeee704fd21031689
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-03-15 23:31:00 +0530

    Create course.cpp

---
+++ 210. Course Schedule II/course.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> visited(numCourses,0);
+        
+        //Make Adj List
+        unordered_map<int,list<int>> adj;
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        //Make Indegree
+        vector<int> inDegree(numCourses,0);
+        for(int i=0;i<numCourses;i++)
+        {
+            for(auto j:adj[i])
+            {
+                inDegree[j]++;
+            }
+        }
+        //Find For 0 indegree nodes
+        queue<int> q;
+
+        for(int i=0;i<numCourses;i++)
+        {
+            if(inDegree[i] == 0)
+            q.push(i);
+        }
+       
+       //Reduce the INDEGREE
+    vector<int> ans;
+       while(!q.empty())
+       {
+           int frontNode=q.front();
+           ans.push_back(frontNode);
+           q.pop();
+           for(auto i:adj[frontNode])
+           {
+               inDegree[i]--;
+               if(inDegree[i] == 0)
+               q.push(i);
+           }
+       }
+       if(ans.size() == numCourses)
+       return ans;
+       else{
+           vector<int> a;
+           return a;
+       }
+    }
+};
