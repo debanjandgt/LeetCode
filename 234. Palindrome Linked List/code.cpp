dac999bc5c27865f01f0d414d05401019dab260c
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2024-03-22 23:35:53 +0530

    Create code.cpp

---
+++ 234. Palindrome Linked List/code.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        int i=0,j=v.size()-1;
+        while(i<j){
+            if(v[i]!=v[j])
+            {
+                return(false);
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
