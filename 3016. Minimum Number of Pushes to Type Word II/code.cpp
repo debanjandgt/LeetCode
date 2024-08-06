0ae16bb58aeecbb4cea3915e580b2f5b456def6f
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2024-08-06 23:48:15 +0530

    Create code.cpp

---
+++ 3016. Minimum Number of Pushes to Type Word II/code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<char, int> um;
+        for (char c : word) {
+            um[c]++;
+        }
+        vector<int> count;
+        for (auto x : um) {
+            count.push_back(x.second);
+        }
+        sort(count.begin(), count.end(), greater<int>());
+        int ans = 0, row = 1;
+     for (int i = 0; i < count.size(); i++) {
+            if (i > 7 && i % 8 == 0) {
+                row++;
+            }
+            ans += row * count[i];
+        }
+        return ans;
+    }
+};
